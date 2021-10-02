4638e901d4a77efa18660228b4dd22a08e44f28e
Raphael Martins <raphael.martins@injunior.com.br>
2021-10-02 00:28:09 -0300

    Add loop index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
-    <h1> Página index.php </h1>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <h1> <?php the_title(); ?> </h1>
+    <?php the_content(); ?>
+
+    <?php endwhile; else ?>
+        <p> <?php _e('Não tem posts com esses critérios.'); ?> </p>
+    <?php endif; ?>
 <?php get_footer(); ?>
